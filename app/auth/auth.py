e1cf1d2a7608f8f9eb092063f6175da3bef6a634
Márcio Xerém <joaopedroscpaim@gmail.com>
2026-05-09 15:40:34 -0300

    alteração de auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,6 +1,11 @@
-from fastapi import Header, HTTPException
 
-API_KEY = "123456"
+from fastapi import Header, HTTPException
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
 
 def verificar_token(authorization: str = Header(None)):
     if not authorization:
@@ -13,7 +18,17 @@ def verificar_token(authorization: str = Header(None)):
 
     return token
 
-def verificar_api_key(x_api_key: str = Header(None)):
-    if x_api_key != API_KEY:
-        raise HTTPException(status_code=403, detail="API Key inválida")
+def validar_api_key(x_api_key: str = Header(None)):
+    if not x_api_key:
+        raise HTTPException(
+            status_code=400,
+            detail="Header X-API-Key é obrigatório"
+        )
 
+    if x_api_key != API_KEY:
+        raise HTTPException(
+            status_code=403,
+            detail="API Key inválida"
+        )
+
+    return x_api_key
